b7e4a90b60ede5c213099df73339ddf5c78001d9
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-02-12 16:34:41 -0300

    A001

--- AprendendoC#/A001 - PrimeiroCodigo/A001 - PrimeiroCodigo/Program.cs
+++ AprendendoC#/A001 - PrimeiroCodigo/A001 - PrimeiroCodigo/Program.cs
@@ -10,6 +10,8 @@ namespace A001___PrimeiroCodigo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
