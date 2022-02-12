bc29d8f401158c52fe97336edc334d2fc4ca66fb
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-02-12 16:52:39 -0300

    A001

--- AprendendoC#/A001 - PrimeiroCodigo/A001 - PrimeiroCodigo/Program.cs
+++ AprendendoC#/A001 - PrimeiroCodigo/A001 - PrimeiroCodigo/Program.cs
@@ -10,8 +10,21 @@ namespace A001___PrimeiroCodigo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            //tipos de variáveis: 
+            //Int, Float, Bool, String e Char
+
+            String entrada = "Hello World!";
+           
+            Console.Write(entrada + "\n"); //mostra os valores, mas sem quebra de linha
+            Console.WriteLine("Meu nome é Nikson"); //mostra os valores com quebra de linha
+
+            //alterar valor da variável: 
+
+            entrada = "Troquei o valor da variável ";
+            Console.Write(entrada + "\n");
+
+
+            Console.ReadLine(); //entrada de dados
         }
     }
 }
