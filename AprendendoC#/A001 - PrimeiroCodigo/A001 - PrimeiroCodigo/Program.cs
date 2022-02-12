a17b2448cbc112b5f810a188b057ec5f34d08638
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-02-12 16:32:54 -0300

    A001

---
+++ AprendendoC#/A001 - PrimeiroCodigo/A001 - PrimeiroCodigo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A001___PrimeiroCodigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
