2025ca366c63bb7a06770bb276a5ce3170373260
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-03-12 17:26:05 -0300

    ok

---
+++ AulasFacul/Projetos/AplicaçãoSite-Parte1/AplicaçãoSite-Parte1/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AplicaçãoSite_Parte1.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar() //para view Cadastrar 
+        {
+            return View();
+        }
+    }
+}
---
+++ AulasFacul/Projetos/AplicaçãoSite-Parte1/AplicaçãoSite-Parte1/Models/ProdutoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicaçãoSite_Parte1.Models
+{
+    public class ProdutoModel
+    {
+        public int Codigo { get; set; }
+        public decimal Valor{ get; set; }
+        public decimal Nome { get; set; }
+    }
+}
