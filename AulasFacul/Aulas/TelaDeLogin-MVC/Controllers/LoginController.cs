2b6a4ad57a8ab0575d9f57a37ee66f1cfe211a99
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-04-02 22:38:04 -0300

    Tela de login com MVC

---
+++ AulasFacul/Aulas/TelaDeLogin-MVC/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelaDeLogin_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Entrar()
+        {
+            return View();
+        }
+        public IActionResult Logado()
+        {
+            return View();
+        }
+        public IActionResult NaoLogado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(string usuario, string senha)
+        {
+            if(usuario == "admin" && senha == "admin")
+            {
+                return RedirectToAction("Logado");
+            }
+            else
+            {
+                return RedirectToAction("NaoLogado");
+            }
+        }
+       
+    }
+}
