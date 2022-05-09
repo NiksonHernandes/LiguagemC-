e5b29986be79ac4566c72e3dacbdb70021ef4c83
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-05-08 21:16:20 -0300

    Atualização

---
+++ AulasFacul/Aulas/TelaDeLogin-MVC/Controllers/ListaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelaDeLogin_MVC.Models;
+
+namespace TelaDeLogin_MVC.Controllers
+{
+    public class ListaController : Controller
+    {
+        public static List<ListaModel> lsLista = new List<ListaModel>();
+
+        public IActionResult Index()
+        {
+            return View(lsLista);
+        }
+
+        public IActionResult AdicionarUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult DeletarUsuario(int id)
+        {
+            lsLista.Remove(lsLista.Find(a => a.Codigo == id));
+            //return View();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarUsuario(ListaModel obj)
+        {
+            lsLista.Add(obj);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ AulasFacul/Aulas/TelaDeLogin-MVC/Models/ListaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelaDeLogin_MVC.Models
+{
+    public class ListaModel
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Descrição { get; set; }
+        public int Codigo { get; set; }
+    }
+}
